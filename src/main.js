b9a265951301d2e7ff36e2aa6437338b182f4c99
Martin Muda <mud@mrecic.gov.ar>
2022-08-23 12:38:12 -0300

    fix: entry point

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
-import zircle from './index.js'
+import zircle from 'zircle'
+import 'zircle/dist/zircle.css'
 
 Vue.use(zircle)
 Vue.config.productionTip = false
